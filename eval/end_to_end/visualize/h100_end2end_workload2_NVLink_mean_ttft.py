0b019fa24b3a1c019fb370c34f302941790d57db
KuntaiDu <kuntai@uchicago.edu>
2025-04-15 03:21:37 +0000

    add NVLink setup visualization
    Signed-off-by: KuntaiDu <kuntai@uchicago.edu>

---
+++ eval/end_to_end/visualize/h100_end2end_workload2_NVLink_mean_ttft.py
@@ -0,0 +1,60 @@
+
+import matplotlib.pyplot as plt
+import json
+from pathlib import Path
+import pandas as pd
+
+from visualize_utils import *
+
+
+
+
+
+
+prefill = pd.DataFrame.from_dict(load_results("../results-0413-2-H100/prefill_csjf"))
+pp = pd.DataFrame.from_dict(load_results("../results-0414-2-H100-NVLink/pp"))
+tp = pd.DataFrame.from_dict(load_results("../results-0414-2-H100-NVLink/tp"))
+
+prefill.sort_values(by="qps", inplace=True)
+pp.sort_values(by="qps", inplace=True)
+tp.sort_values(by="qps", inplace=True)
+
+
+fig, ax = plt.subplots(figsize=(10, 5))
+
+ax.plot(prefill["qps"], 
+        prefill["mean_ttft_ms"] / 1000, 
+        label=method2name["prefill"], 
+        color=colors["prefill"],
+        linewidth=2,
+        marker='v',
+        markersize=10)
+ax.plot(pp["qps"], 
+        pp["mean_ttft_ms"] / 1000, 
+        label=method2name["pp"], 
+        color=colors["pp"],
+        linewidth=2,
+        marker='v',
+        markersize=10)
+ax.plot(tp["qps"], 
+        tp["mean_ttft_ms"] / 1000, 
+        label=method2name["tp"], 
+        color=colors["tp"],
+        linewidth=2,
+        marker='v',
+        markersize=10)
+ax.legend(loc="lower right", framealpha=0.7)
+ax.grid()
+ax.set_xlabel("Query per second")
+ax.set_ylabel("Mean latency (s)")
+ax.set_ylim(bottom=0)
+ax.set_ylim(top=140)
+
+bbox_props = dict(boxstyle="rarrow", fc=(1,1,1), ec="grey", lw=2)
+ax.text(0.05, 0.95, "Better", ha="left", 
+va="top", rotation=-45, bbox=bbox_props, c='grey',
+transform=ax.transAxes
+)
+
+fig.tight_layout()
+fig.savefig("figures/h100_end2end_workload2_NVLink_mean_ttft.pdf", bbox_inches="tight")
--- eval/end_to_end/visualize/plot_all.sh
+++ eval/end_to_end/visualize/plot_all.sh
@@ -18,3 +18,17 @@ python h100_end2end_workload2_p99_ttft.py
 echo "Plotting WL2 H100 tput..."
 python h100_end2end_workload2_tput.py
 
+echo "Plotting WL1 H100 NVLink mean ttft..."
+python h100_end2end_workload1_NVLink_mean_ttft.py
+
+echo "Plotting WL1 H100 NVLink p99 ttft..."
+python h100_end2end_workload1_NVLink_p99_ttft.py
+
+echo "Plotting WL2 H100 NVLink mean ttft..."
+python h100_end2end_workload2_NVLink_mean_ttft.py
+
+echo "Plotting WL2 H100 NVLink p99 ttft..."
+python h100_end2end_workload2_NVLink_p99_ttft.py
+
+echo "Plotting WL2 H100 NVLink tput..."
+python h100_end2end_workload2_NVLink_tput.py
